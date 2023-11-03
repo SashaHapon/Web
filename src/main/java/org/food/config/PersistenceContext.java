6864b86b744554447bb1b1a874a14553e37db092
Brotherick <full8back@gmail.com>
2023-11-03 07:45:30 +0300

    mysql: update

--- src/main/java/org/food/config/PersistenceContext.java
+++ src/main/java/org/food/config/PersistenceContext.java
@@ -76,7 +76,7 @@ public class PersistenceContext {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         return properties;
     }
