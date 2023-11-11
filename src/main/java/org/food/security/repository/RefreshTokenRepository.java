a1a080a5ae9f52abfcfe2a5a58ea285348a1b122
Brotherick <full8back@gmail.com>
2023-11-11 12:56:30 +0300

    1

---
+++ src/main/java/org/food/security/repository/RefreshTokenRepository.java
@@ -0,0 +1,19 @@
+package org.food.security.repository;
+
+import java.util.Optional;
+
+import org.food.model.RefreshToken;
+import org.food.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
