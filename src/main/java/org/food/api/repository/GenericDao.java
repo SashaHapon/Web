81205c135d454108ff0320d19b541ba30fd5ebc8
Brotherick <full8back@gmail.com>
2023-11-27 19:13:26 +0300

    fix

--- src/main/java/org/food/api/repository/GenericDao.java
+++ src/main/java/org/food/api/repository/GenericDao.java
@@ -1,5 +1,7 @@
 package org.food.api.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+
 import java.util.List;
 
 public interface GenericDao<T> {
@@ -14,5 +16,6 @@ public interface GenericDao<T> {
 
     List<T> findAll(int id, int limit);
 
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, value = "order_entity_graph")
     T findOrderByIdWithEntityGraph(Integer id);
 }
